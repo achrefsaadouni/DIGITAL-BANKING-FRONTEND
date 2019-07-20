616b562886703702d591f8266a7d385caaa67b70
achref <achref.saadouni@esprit.tn>
2019-07-21 00:02:20 +0100

    updating backend template

--- src/app/private/layout/layout.component.ts
+++ src/app/private/layout/layout.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {NgxSpinnerService} from 'ngx-spinner';
 import {AuthService} from '../../public/shared/auth.service';
+import {FormControl} from '@angular/forms';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
+  mode = new FormControl('over');
+  showFiller = false;
   constructor(private service: AuthService, private spinner: NgxSpinnerService) {
   }
 
