bf821493ec09b4fa2a1c5ddae92508ffed33860c
achref <achref.saadouni@esprit.tn>
2019-07-21 17:55:39 +0100

    updating template

--- src/app/private/layout/layout.component.ts
+++ src/app/private/layout/layout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NgxSpinnerService} from 'ngx-spinner';
 import {AuthService} from '../../public/shared/auth.service';
 import {FormControl} from '@angular/forms';
+import {User} from '../../public/shared/models/User';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -10,7 +11,9 @@ import {FormControl} from '@angular/forms';
 export class LayoutComponent implements OnInit {
   mode = new FormControl('over');
   showFiller = false;
+  user: User;
   constructor(private service: AuthService, private spinner: NgxSpinnerService) {
+    this.user = this.service.getUser();
   }
 
   ngOnInit() {
